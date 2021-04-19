72c42c05d3e20f87b802dba65f9951a5256f72d9
雷世虹 <lsh960529@gmail.com>
2021-04-19 14:27:21 +0800

    init

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,18 +1,4 @@
 module.exports = {
-	// 使能每一种语言默认格式化规则
-	'[html]': {
-		'editor.defaultFormatter': 'esbenp.prettier-vscode'
-	},
-	'[css]': {
-		'editor.defaultFormatter': 'esbenp.prettier-vscode'
-	},
-	'[less]': {
-		'editor.defaultFormatter': 'esbenp.prettier-vscode'
-	},
-	'[javascript]': {
-		'editor.defaultFormatter': 'esbenp.prettier-vscode'
-	},
-
 	tabWidth: 2, // tab缩进大小,默认为2
 	useTabs: true, // 使用tab缩进，默认false
 	semi: false, // 使用分号, 默认true
@@ -21,9 +7,9 @@ module.exports = {
 	bracketSpacing: true, // 对象中的空格 默认true
 	jsxBracketSameLine: false, // JSX标签闭合位置 默认false 在jsx中把'>' 单独放一行
 	arrowParens: 'avoid', // 箭头函数参数括号，默认avoid 可选 avoid| always,avoid 能省略括号的时候就省略 例如x => x，always 总是有括号
-	htmlWhitespaceSensitivity: 'strict', //html空格严格程度，可选<css|strict|ignore>
+	htmlWhitespaceSensitivity: 'ignore', //html空格严格程度，可选<css|strict|ignore>
 	proseWrap: 'preserve', // 默认值。因为使用了一些折行敏感型的渲染器（如GitHub comment）而按照markdown文本样式进行折行
-	requireConfig: false // Require a 'prettierconfig' to format prettier
+	requireConfig: false, // Require a 'prettierconfig' to format prettier
 
 	// trailingComma: 'es5' // 在对象或数组最后一个元素后面是否加逗号（在ES5中加尾逗号）
 	// tslintIntegration": false // 不让prettier使用tslint的代码格式进行校验
@@ -31,8 +17,8 @@ module.exports = {
 	// jsxSingleQuote": false, // 在jsx中使用单引号代替双引号
 	// parser": "babylon", // 格式化的解析器，默认是babylon
 
-	// insertPragma: false,
-	// printWidth: 120,
-	// quoteProps: 'as-needed',
-	// requirePragma: false
+	insertPragma: false,
+	printWidth: 120,
+	quoteProps: 'as-needed',
+	requirePragma: false
 }
--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,9 @@
 
 <style >
 #app {
-		margin-top: 60px;
-		color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-
 }
 
 </style>
