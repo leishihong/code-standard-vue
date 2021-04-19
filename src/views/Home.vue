a50748eff3f857ded2afe99f5f13723d3e6a0e60
雷世虹 <lsh960529@gmail.com>
2021-04-19 14:51:03 +0800

    feat: init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
 	<div class="home">
 		<img alt="Vue logo" src="../assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<HelloWorld
+			msg="Welcome to Your Vue.js App"
+			class="ceshi dede dede de"
+			style="margin-top: 20px; text-align: center"
+		/>
 	</div>
 </template>
 
