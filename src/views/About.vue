63954a6f2574984c3305c062ed61f4e3c5b8c6d4
雷世虹 <lsh960529@gmail.com>
2021-04-19 15:08:10 +0800

    feat: update p

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
 	<div class="about">
 		<h1>This is an about page</h1>
+		<p>ceshi21313</p>
 	</div>
 </template>
+<style>
+.about {
+	text-align: center;
+	color: red;
+	padding: 20px;
+	margin: 20px;
+}
+</style>
